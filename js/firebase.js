d81de721c3cbf26578d73eced8530ddfb38544e3
Ramon <r.baertschi@prc-solutions.ch>
2017-10-31 15:31:03 +0100

    User Info abfragen hinzugefügt

--- js/firebase.js
+++ js/firebase.js
@@ -28,5 +28,12 @@ function signIn() {
 }
 
 function showName(){
+
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture : imageUrl
+      });
+      
     document.getElementById("welcomUser").innerHTML = "Willkommen " + user.displayName;
 }
